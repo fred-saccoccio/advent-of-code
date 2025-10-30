155491c6a6e8da225b062941e7cc9066e8107c1d
fred-saccoccio <frederic.saccoccio@gmail.com>
2025-10-30 15:19:54 +0100

    First working machine. Added flex+bison sources.

--- 2015/day.07/WireMachine.cpp
+++ 2015/day.07/WireMachine.cpp
@@ -5,47 +5,94 @@ using namespace std;
 #include "WireMachine.h"
 
 void WireMachine::AddWireNode(WireNode w) {
-  // Try to add the node in the internal dictionnary
-  if(this->_Dictionnary.find(w.Name) != this->_Dictionnary.end()) {
-    throw "Error already exist";
-  }
-  
-  this->_Dictionnary[w.Name] = w;
-
+  /*
   // Check if it aready exists
-  // If it references other WireNodes, check if they previously exist
+  if(this->_dict.find(w.Name) != this->_dict.end()) {
+    char buff[128];
+    snprintf(buff, sizeof(buff), "AddWireNode(...): '%s' wire already exists", w.Name.c_str());
+    std::string buffAsStdStr = buff;
+    throw buffAsStdStr;
+  }
+  // If it references other WireNode(s), check if it(they) previously exist(s)
+  int nb_refs_to_check = 0;
+  switch(w.Type) {
+    case IMMEDIATE:
+      nb_refs_to_check = 0;
+      break;
+    case OP_NOT:
+    case OP_SHIFTR:
+    case OP_SHIFTL:
+      nb_refs_to_check = 1;
+      break;
+    case OP_AND:
+    case OP_OR:
+      nb_refs_to_check = 2;
+      break;
+  }
+
+  for(int i = 0; i < nb_refs_to_check; i++) {
+    if(this->_dict.find(w.Connections[i]) == this->_dict.end()) {
+      char buff[128];
+      snprintf(buff, 
+          sizeof(buff), "AddWireNode(.Name='%s'): referenced wire '%s' not found", 
+          w.Name.c_str(),
+          w.Connections[i].c_str());
+      std::string buffAsStdStr = buff;
+      throw buffAsStdStr;
+    } 
+  }
+  */
+
+  this->_dict[w.Name] = w;
 }
 
-short WireMachine::QueryWireNodeValue(string name) {
-  short retVal = 0;
+unsigned short WireMachine::QueryWireNodeValue(string name) {
+  unsigned short retVal = 0;
   // Check if the WireNode is immediate
   // Otherwise, recursively call this method
-  if(this->_Dictionnary.find(name) == this->_Dictionnary.end()) {
+  if(this->_dict.find(name) == this->_dict.end()) {
     char buff[128];
     snprintf(buff, sizeof(buff), "QueryWireNodeValue('%s'): wire not found", name.c_str());
     std::string buffAsStdStr = buff;
     throw buffAsStdStr;
   }
-  WireNode w = this->_Dictionnary[name];
+  WireNode w = this->_dict[name];
 
   switch(w.Type) {
+    case IDLE:
+      {
+        char buff[128];
+        snprintf(buff, sizeof(buff), "QueryWireNodeValue('%s'): idle", w.Name.c_str());
+        std::string buffAsStdStr = buff;
+        throw buffAsStdStr;
+      }
+      break;
     case IMMEDIATE:
       retVal = w.Value; 
+      break;
+    case SINGLE:
+      retVal = this->QueryWireNodeValue(w.Connections[0]);
       break;
     case OP_NOT:
       retVal = ~this->QueryWireNodeValue(w.Connections[0]);
       break;
-    case OP_SHIFTR:
-      retVal = this->QueryWireNodeValue(w.Connections[0]) >> w.Shift;
+    case OP_SHIFT_R:
+      retVal = this->QueryWireNodeValue(w.Connections[0]) >> w.Value;
       break;
-    case OP_SHIFTL:
-      retVal = this->QueryWireNodeValue(w.Connections[0]) << w.Shift;
+    case OP_SHIFT_L:
+      retVal = this->QueryWireNodeValue(w.Connections[0]) << w.Value;
       break;
-    case OP_AND:
+    case OP_AND_2_OPS:
       retVal = this->QueryWireNodeValue(w.Connections[0]) & this->QueryWireNodeValue(w.Connections[1]) ;
       break;
-    case OP_OR:
+    case OP_AND_1_OP:
+      retVal = this->QueryWireNodeValue(w.Connections[0]) & w.Value;
+      break;
+    case OP_OR_2_OPS:
       retVal = this->QueryWireNodeValue(w.Connections[0]) | this->QueryWireNodeValue(w.Connections[1]) ;
+      break;
+    case OP_OR_1_OP:
+      retVal = this->QueryWireNodeValue(w.Connections[0]) | w.Value;
       break;
   }
 
