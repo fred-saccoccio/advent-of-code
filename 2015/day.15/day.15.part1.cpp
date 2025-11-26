27728bb52e20cecd8aef83e3a93eef3cb0109238
fred-saccoccio <frederic.saccoccio@gmail.com>
2025-11-26 22:06:49 +0100

    Day #15 : added a function template to enumerate all the subsets of a set.

--- 2015/day.15/day.15.part1.cpp
+++ 2015/day.15/day.15.part1.cpp
@@ -49,6 +49,55 @@ class StringSplitter {
     }
 };
 
+void printVector(vector<int> v) {
+  printf("[");
+
+  for(size_t i = 0; i < v.size(); i++) {
+    printf("%d", v[i]);
+    if(i != v.size() - 1) {
+      printf(",");
+    }
+  }
+
+  printf("]");
+}
+
+template <typename T> vector<vector<T>> getSubsets(vector<T>& v) {
+  vector<vector<T>> retVal;  
+  if(v.size() == 0) {
+    vector<T> empty;
+    retVal.push_back(empty);
+    return retVal;
+  }
+
+  // NOTE : v.size() > 0 at this point
+  
+  // Recursively call getSubsets on v minus its last element
+  vector<T> recCallSet = { v.begin(), v.end()-1 };
+  vector<vector<T>> recCallSubsets = getSubsets(recCallSet);
+
+  // Build the return value by combining :
+  // 1. The recursive call on the set of v's n-1 first elements
+  // 2. All the elements of the recursive call where we add v's last element
+  for(auto& e:recCallSubsets) {
+    retVal.push_back(e);
+  }
+ 
+  int elt = v.back();
+  for(auto& e:recCallSubsets) {
+    vector<int> a = e;
+    a.push_back(elt);
+    retVal.push_back(a);
+  }
+
+  return retVal;
+}
+
+vector<vector<unsigned int>> getPartitions(unsigned int n) {
+  vector<vector<unsigned int>> retVal;
+  return retVal;
+}
+
 int main (int argc, char *argv[]) {
   
   vector<Ingredient> ingredients;
@@ -91,6 +140,26 @@ int main (int argc, char *argv[]) {
   for(auto& i:ingredients) {
     i.println();
   }
+
+  vector<int> data;
+  for(int i = 0; i < 6; i++) {
+    data.push_back(i);
+  }
+  printVector(data);
+  printf("\n");
+  printf("data.size()=%zu\n", data.size());
+
+  auto subsets = getSubsets<int>(data);
+  printf("[");
+  printf("\n");
+  for(auto& s:subsets) {
+    printf("  ");
+    printVector(s);
+    printf("\n");
+  }
+  printf("]");
+  printf("\n");
+  printf("subsets.size()=%zu\n", subsets.size());
 
   return 0;
 }
