86a85f3fb6b16b8670c1f36128c12344e93b0da0
fred-saccoccio <frederic.saccoccio@gmail.com>
2025-12-14 20:55:24 +0100

    Day #7 : processing data and computing beams through the grid

---
+++ 2025/day.07/day.07.part1.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main (int argc, char *argv[]) {
+  vector<vector<char>> grid;
+
+  // Read input data
+  string input;
+  while(getline(cin, input)) {
+    vector<char> v;
+    for(char c:input) {
+      v.push_back(c);
+    }
+    grid.push_back(v);
+  }
+
+  // Process start line : add a beam under the 'S' char
+  size_t index = 0;
+  
+  for(char c:grid[0]) {
+    if(c == 'S')
+      break;
+    index++;
+  }
+  
+  if(index == grid[0].size())
+    cout << "No start (S) in the first line\n";
+  
+  grid[1][index] = '|';
+
+  size_t LINES = grid.size();
+  size_t COLS = grid[0].size();
+  for(size_t line = 1; line < LINES-1; line++) {
+    for(size_t col = 0; col < COLS; col++) {
+      // Beam here ?
+      if(grid[line][col] == '|') {
+        if(grid[line+1][col] == '.' ) {
+          // Beam moves forward
+          grid[line+1][col] = '|'; 
+        } else if(grid[line+1][col] == '^' ) {
+          // Split the beam : Left
+          if((int)col-1 >= 0) {
+            grid[line+1][col - 1] = '|';
+          }
+          // Split the beam : Left
+          if(col+1 < COLS) {
+            grid[line+1][col + 1] = '|';
+          }
+        }
+      }
+    }
+  }
+
+  return 0;
+}
