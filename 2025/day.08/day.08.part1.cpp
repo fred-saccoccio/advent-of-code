0478e60d039d1a7ef42a89c866beb6bd11f1e32f
fred-saccoccio <frederic.saccoccio@gmail.com>
2025-12-28 23:13:44 +0100

    Day #8: added sorting

---
+++ 2025/day.08/day.08.part1.cpp
@@ -0,0 +1,156 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Point3d {
+  int X;
+  int Y;
+  int Z;
+  int Circuit;
+
+  void print() {
+    printf("{X=%8d,Y=%8d,Z=%9d,Circuit=%4d}", X, Y, Z, Circuit);
+  }
+
+  void println() {
+    print();
+    printf("\n");
+  }
+};
+
+struct JunctionBoxesDist {
+  size_t Line;
+  size_t Col;
+  double Dist;
+  
+  bool operator<(const JunctionBoxesDist& other) const {
+    return Dist < other.Dist;
+  }
+};
+
+double distance(Point3d p, Point3d q) {
+  return sqrt((q.X-p.X)*(q.X-p.X) + (q.Y-p.Y)*(q.Y-p.Y) + (q.Z-p.Z)*(q.Z-p.Z));
+}
+
+void print_distances(double **d, size_t s) {
+  for(size_t i = 0; i < s; i++) {
+    printf("%8zu", i);
+    if(i < s-1)
+      printf(" ");
+  }
+  printf("\n");
+  
+  for(size_t i = 0; i < s; i++) {
+    printf("--------");
+    if(i < s-1)
+      printf("-");
+  }
+  printf("\n");
+
+  for(size_t line = 0; line < s; line++) {
+    for(size_t col = 0; col < s; col++) {
+      printf("%8.2f", d[line][col]);
+      if(col < s-1)
+        printf(" ");
+    }
+    printf("\n");
+  }
+}
+
+class StringSplitter {
+  public:
+    static std::vector<std::string> split(const std::string& str,
+        const std::string& delimiters,
+        bool keepEmpty = false) {
+      std::vector<std::string> tokens;
+      std::string::size_type pos = 0;
+      std::string::size_type prev = 0;
+      
+      while ((pos = str.find_first_of(delimiters, prev)) != std::string::npos) {
+        if (keepEmpty || pos > prev) {
+          tokens.push_back(str.substr(prev, pos - prev));
+        }
+        prev = pos + 1;
+      }
+      
+      if (prev < str.length()) {
+        tokens.push_back(str.substr(prev));
+      } else if (keepEmpty && prev == str.length()) {
+        tokens.push_back("");
+      }
+      
+      return tokens;
+    }
+};
+
+const size_t MAX_SIZE=1000;
+
+int main (int argc, char *argv[]) {
+  double** distances;
+  distances = new double*[MAX_SIZE];
+  for(size_t i = 0; i < MAX_SIZE; i++) {
+    distances[i] = new double[MAX_SIZE];
+  }
+  Point3d* boxes = new Point3d[MAX_SIZE];
+
+  // Read input data
+  string input;
+  size_t line_index = 0;
+  size_t lines = 0;
+  while(getline(cin, input)) {
+    // Split the line with coma as separator
+    auto splits = StringSplitter::split(input, ",");
+    int x = stoi(splits[0]);
+    int y = stoi(splits[1]);
+    int z = stoi(splits[2]);
+
+    boxes[line_index].X = x;
+    boxes[line_index].Y = y;
+    boxes[line_index].Z = z;
+    boxes[line_index].Circuit = line_index;
+
+    if(line_index >= MAX_SIZE-1)
+      break;
+
+    line_index++;
+  }
+
+  lines = line_index;
+
+  for(size_t i = 0; i < lines; i++) {
+    printf("[%4zu]=", i);
+    boxes[i].println();
+  }
+
+  vector<JunctionBoxesDist> dists;
+  double min_distance = DBL_MAX;
+  size_t line_min, col_min;
+  for(size_t line = 0; line < lines; line++) {
+    for(size_t col = line+1; col < lines; col++) {
+      double val = distance(boxes[line], boxes[col]);
+      distances[line][col] = val;
+      JunctionBoxesDist jb;
+      jb.Line = line;
+      jb.Col = col;
+      jb.Dist = val;
+      dists.push_back(jb);
+      if(val < min_distance) {
+        min_distance = val;
+        line_min = line;
+        col_min = col;
+      }
+    }
+  }
+
+  sort(dists.begin(), dists.end());
+
+  print_distances(distances, lines);
+
+  printf("min=%f (%zu,%zu)\n", min_distance, line_min, col_min);
+  
+  delete [] boxes;
+  for(size_t i = 0; i < MAX_SIZE; i++) {
+    delete [] distances[i];
+  }
+  delete [] distances;
+}
+
